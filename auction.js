9ec5e8346e4affbeea8c44e451422972cc6437e4
NewCompte <victorvvv@gmail.com>
2015-06-14 12:06:01 +0200

    Indentation + minimum bid is 1.

--- auction.js
+++ auction.js
@@ -25,6 +25,7 @@ var bidTime = 25000;
 var additionTime = 10000;
 var lock = 0;
 var bidTimeout = 0;
+var minimumBid = 1;
 
 
 
@@ -106,32 +107,30 @@ if (Meteor.isClient) {
       }
   })
 
-  Template.rosters.helpers(
-    {
-      divisions : function() {
-        return Divisions.find({},{sort:{order:1}});
-      },
-      teams : function(division) {
-        return TeamNames.find({"division" : division}, {sort:{order:1}});
-      },
-      teamID: function(teamname) {
-        return teamname.split(" ").join("_");
-      },
-      players : function(teamname) {
-        return TeamData.find({"teamname" : teamname}, {sort : {order : 1}});
-      },
-      inRedDivision : function(division) {
-        if(division == "Central" || division == "Pacific") {
-          return "rgb(235, 126, 126)";
-        }
-        else {
-          return "rgb(134, 198, 230)";
-        }
-      },
+  Template.rosters.helpers({
+    divisions : function() {
+      return Divisions.find({},{sort:{order:1}});
+    },
+    teams : function(division) {
+      return TeamNames.find({"division" : division}, {sort:{order:1}});
+    },
+    teamID: function(teamname) {
+      return teamname.split(" ").join("_");
+    },
+    players : function(teamname) {
+      return TeamData.find({"teamname" : teamname}, {sort : {order : 1}});
+    },
+    inRedDivision : function(division) {
+      if(division == "Central" || division == "Pacific") {
+        return "rgb(235, 126, 126)";
+      }
+      else {
+        return "rgb(134, 198, 230)";
+      }
+    },
   });
 
-  Template.get_funds.helpers(
-  {
+  Template.get_funds.helpers({
     getBalance: function () {
       if( Meteor.user() !== undefined && Meteor.userId()) {
         return TeamNames.findOne({"captain":Meteor.user().username}).money;
@@ -152,105 +151,97 @@ if (Meteor.isClient) {
   });
 
   // display_time (aka bid status)
-  Template.display_time.helpers(
-    {
-      time : function() {
-        if(AuctionData.findOne({}) !== undefined) {
-            var curtime = Session.get('time') + Session.get('serverTimeOffset');
-            var remainingTime = AuctionData.findOne({}).nextExpiryDate;
-            var secsLeft = Math.ceil((remainingTime - curtime)/100)/10;
-            if(secsLeft < 0) {
-              //Meteor.call("checkForToggle");
-            }
-            else {
-              formattedSeconds = String(secsLeft)
-              if(parseInt(secsLeft) == secsLeft) {
-                formattedSeconds += ".0";
-              }
-              if(secsLeft < 10) {
-                formattedSeconds = "0"+formattedSeconds;
-              }
-              return formattedSeconds;
-            }
+  Template.display_time.helpers({
+    time : function() {
+      if(AuctionData.findOne({}) !== undefined) {
+          var curtime = Session.get('time') + Session.get('serverTimeOffset');
+          var remainingTime = AuctionData.findOne({}).nextExpiryDate;
+          var secsLeft = Math.ceil((remainingTime - curtime)/100)/10;
+          if(secsLeft < 0) {
+            //Meteor.call("checkForToggle");
           }
-      },
-      currentNominatingPlayer : function() {
-        if(AuctionData.findOne({}) !== undefined)
-          return AuctionData.findOne({}).Nominator;
-      },
-      personBeingBidOn : function() {
-        if(AuctionData.findOne({}) !== undefined) {
-          name = AuctionData.findOne({}).currentPlayer;
-          currentPlayerInfo = PlayerResponse.findOne({"tagpro":name});
-          return name;
+          else {
+            formattedSeconds = String(secsLeft)
+            if(parseInt(secsLeft) == secsLeft) {
+              formattedSeconds += ".0";
+            }
+            if(secsLeft < 10) {
+              formattedSeconds = "0"+formattedSeconds;
+            }
+            return formattedSeconds;
+          }
         }
-      },
-      currentPlayerInfo: function() {
-        if(AuctionData.findOne({}) !== undefined) {
-        return PlayerResponse.findOne({"tagpro":AuctionData.findOne({}).currentPlayer});
+    },
+    currentNominatingPlayer : function() {
+      if(AuctionData.findOne({}) !== undefined)
+        return AuctionData.findOne({}).Nominator;
+    },
+    personBeingBidOn : function() {
+      if(AuctionData.findOne({}) !== undefined) {
+        name = AuctionData.findOne({}).currentPlayer;
+        currentPlayerInfo = PlayerResponse.findOne({"tagpro":name});
+        return name;
       }
-      },
-      bidAmount :function()
-      {
-        if(AuctionData.findOne({}) !== undefined)
-          return AuctionData.findOne({}).currentBid;
-      },
-      lastBidder : function()
-      {
-        if(AuctionData.findOne({}) !== undefined)
-          return AuctionData.findOne({}).lastBidder;
-      },
-      isNominationTime: function()
-      {
-        if(AuctionData.findOne({}) !== undefined) {
-          auction_state = AuctionData.findOne({}).State;
-          Session.set("nominationTime",  auction_state == "Nominating");
-          return auction_state == "Nominating";
+    },
+    currentPlayerInfo: function() {
+      if(AuctionData.findOne({}) !== undefined) {
+      return PlayerResponse.findOne({"tagpro":AuctionData.findOne({}).currentPlayer});
+    }
+    },
+    bidAmount :function() {
+      if(AuctionData.findOne({}) !== undefined)
+        return AuctionData.findOne({}).currentBid;
+    },
+    lastBidder : function() {
+      if(AuctionData.findOne({}) !== undefined)
+        return AuctionData.findOne({}).lastBidder;
+    },
+    isNominationTime: function() {
+      if(AuctionData.findOne({}) !== undefined) {
+        auction_state = AuctionData.findOne({}).State;
+        Session.set("nominationTime",  auction_state == "Nominating");
+        return auction_state == "Nominating";
+      }
+    },
+    isTurnToNominate: function() {
+      if(!Meteor.userId())
+        return false;
+      if(AuctionData.findOne({}) !== undefined) {
+        nominator = AuctionData.findOne({}).Nominator;
+        myNomination = Meteor.user().username == nominator;
+        if(myNomination) {
+          Session.set("playSound", "myNomination");
         }
-      },
-      isTurnToNominate: function()
-      {
-        if(!Meteor.userId())
-          return false;
-        if(AuctionData.findOne({}) !== undefined) {
-          nominator = AuctionData.findOne({}).Nominator;
-          myNomination = Meteor.user().username == nominator;
-          if(myNomination) {
-            Session.set("playSound", "myNomination");
-          }
 
-          return myNomination;
-        }
-      },
-      auctionPaused : function() {
-        if(AuctionStatus.findOne({}) !== undefined) {
-          status = AuctionStatus.findOne({}).status;
-          return (status == "Paused" || status == "Not Started");
-        }
-      },
-      auctionMessage: function() 
-      {
-        if(AuctionStatus.findOne({}) !== undefined) {
-          status = AuctionStatus.findOne({}).status;
-          if(status == "Paused")
-            return "Auction is paused"
-          else
-            return "Auction has not started"
-        }
-      },
-      getsnarkymessage: function() 
-      {
-        options = ["Hurry it up ", "We don't have all day, ", "Waiting on you ", "Getting old here, ", "Take your time ", "No rush ", "C'mon ", "Need some help "]
-        idx = Math.floor(Math.random() * options.length)
-        return options[idx];
-      },
-      nextNominator: function() 
-      {
-        nextorder = Nominators.findOne({"name":"nextInOrder"}).nextorder;
-        var nextNominator = Nominators.findOne({"order":nextorder}).name;
-        //console.log
-        return nextNominator;
-      }      
+        return myNomination;
+      }
+    },
+    auctionPaused : function() {
+      if(AuctionStatus.findOne({}) !== undefined) {
+        status = AuctionStatus.findOne({}).status;
+        return (status == "Paused" || status == "Not Started");
+      }
+    },
+    auctionMessage: function() {
+      if(AuctionStatus.findOne({}) !== undefined) {
+        status = AuctionStatus.findOne({}).status;
+        if(status == "Paused")
+          return "Auction is paused"
+        else
+          return "Auction has not started"
+      }
+    },
+    getsnarkymessage: function() {
+      options = ["Hurry it up ", "We don't have all day, ", "Waiting on you ", "Getting old here, ", "Take your time ", "No rush ", "C'mon ", "Need some help "]
+      idx = Math.floor(Math.random() * options.length)
+      return options[idx];
+    },
+    nextNominator: function() {
+      nextorder = Nominators.findOne({"name":"nextInOrder"}).nextorder;
+      var nextNominator = Nominators.findOne({"order":nextorder}).name;
+      //console.log
+      return nextNominator;
+    }      
   });
 
   Template.playSound.helpers({
@@ -302,8 +293,7 @@ if (Meteor.isClient) {
       }
       return true;
     },
-    sufficientFunds: function()
-    {
+    sufficientFunds: function() {
       if(Meteor.user() !== undefined) {
         var team = TeamNames.findOne({"captain":Meteor.user().username});
         var balance = parseInt(team.money);
@@ -323,8 +313,7 @@ if (Meteor.isClient) {
       }
       return false;
     },
-    bids: function()
-    {
+    bids: function() {
       if(AuctionData.findOne({}) !== undefined) {
         var bids = [];
         var currentBid = parseInt(AuctionData.findOne({}).currentBid);
@@ -373,7 +362,7 @@ if (Meteor.isClient) {
     },
     'submit .add-nomination' : function(event) {
       var name = event.target.player.value;
-      Meteor.call("toggleState", name, 0);
+      Meteor.call("toggleState", name, minimumBid);
       return false;
     },
     'submit .set-bidtime' : function(event) {
@@ -395,37 +384,33 @@ if (Meteor.isClient) {
     },
   });
 
-  Template.display_bidding_options.events(
-    {
-      'submit .bid-on-player' : function(event)
-      {
-        var bid = parseInt(event.target.amount.value);
-        if(bid >= 0) {
-          Meteor.call("acceptBid", Meteor.user().username, bid, new Date().getTime());
-        }
-        Session.set("playSound", "bid")
-        return false;
-      },
-      'submit .nominate-player' : function(event)
-      { 
-        var player = event.target.player.value;
-        var bid = parseInt(event.target.amount.value);
-        if(!bid || bid < 0) {
-          bid = 0;
-        }
-        Meteor.call("toggleState", player, bid);
-        return false;
-      },
-      'click .bid-button' : function(event) {
-        var bid = parseInt(event.currentTarget.getAttribute('amount'));
-        if(bid >= 0) {
-          Meteor.call("acceptBid", Meteor.user().username, bid, new Date().getTime());
-        }
-        Session.set("playSound", "bid")
-        return false;
+  Template.display_bidding_options.events({
+    'submit .bid-on-player' : function(event) {
+      var bid = parseInt(event.target.amount.value);
+      if(bid >= 0) {
+        Meteor.call("acceptBid", Meteor.user().username, bid, new Date().getTime());
       }
+      Session.set("playSound", "bid")
+      return false;
+    },
+    'submit .nominate-player' : function(event) { 
+      var player = event.target.player.value;
+      var bid = parseInt(event.target.amount.value);
+      if(!bid || bid < minimumBid) {
+        bid = minimumBid;
+      }
+      Meteor.call("toggleState", player, bid);
+      return false;
+    },
+    'click .bid-button' : function(event) {
+      var bid = parseInt(event.currentTarget.getAttribute('amount'));
+      if(bid >= 0) {
+        Meteor.call("acceptBid", Meteor.user().username, bid, new Date().getTime());
+      }
+      Session.set("playSound", "bid")
+      return false;
     }
-  );
+  });
 
   Template.getmessages.helpers({
     lastXmessages: function(limit) {
@@ -508,20 +493,18 @@ if (Meteor.isClient) {
       }
     }
   });
-  Template.messages.events(
-    {
-      'submit' : function(event)
-      {
-        //console.log("Got a new message from ", Meteor.user().username);
-        //console.log("Message text:", event.target.text.value);
-        if(!Meteor.userId()) {
-         return false;
-        }
-        var text = Meteor.user().username + ": " + event.target.text.value;
-        Meteor.call("insertMessage", text, new Date(), "textMessage");
-        event.target.text.value = "";
-        return false;
-      },
+  Template.messages.events({
+    'submit' : function(event) {
+      //console.log("Got a new message from ", Meteor.user().username);
+      //console.log("Message text:", event.target.text.value);
+      if(!Meteor.userId()) {
+       return false;
+      }
+      var text = Meteor.user().username + ": " + event.target.text.value;
+      Meteor.call("insertMessage", text, new Date(), "textMessage");
+      event.target.text.value = "";
+      return false;
+    },
     'click .delete' : function() {
       Meteor.call("removeMessage", this._id);
     },
