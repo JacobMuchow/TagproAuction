124f2f98ad500cb216688a9629e4979ac28a3989
NewCompte <victorvvv@gmail.com>
2016-03-20 21:09:05 +0100

    empty

--- auction.js
+++ auction.js
@@ -846,7 +846,7 @@ if (Meteor.isServer) {
     console.log("Loading it up");
     // Clear state
 
-    var renewData = true;
+    var renewData = 1;
     var createDataSnake = false;
     var renewDataSnake = false;
     var numDraftSpots = 24;
