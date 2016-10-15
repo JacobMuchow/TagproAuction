3b0e0b5676a32096bc164540937e8e207fbdeb7f
NewCompte <victorvvv@gmail.com>
2016-10-15 17:50:09 +0200

    ter

--- genner.py
+++ genner.py
@@ -35,13 +35,13 @@ for index, data in enumerate(captains):
 
 for index, data in enumerate(managers):
 	captain, team_name, division, additional_money, first_player = data
+	count = 0
 	nominations.append({"name" : captain, "rosterfull" : False, "order" : -1})
-	team_names.append({"teamname":team_name, "division" : division, "money" : starting_money + additional_money, "keepermoney":0, "captain":captain, "numrosterspots":team_size, "count":1, "order":(len(team_names) + 1)})
 	if first_player:
 		teams.append({"name" : first_player, "cocaptain":True, "order" : 1, "cost" : 0, "division" : division, "teamname" : team_name })
-	else:
-		teams.append({"name":"", "order" : 1, "cost" : 0, "division" : division, "teamname" : team_name })
-	for x in range(2, team_size+1):
+		count++
+	team_names.append({"teamname":team_name, "division" : division, "money" : starting_money + additional_money, "keepermoney":0, "captain":captain, "numrosterspots":team_size, "count":count, "order":(len(team_names) + 1)})
+	for x in range(count+1, team_size+1):
 		teams.append({"name":"", "order" : x, "cost" : 0, "division" : division, "teamname" : team_name })
 	keepers.append({"captain" : captain, "keepers":[]})
 
