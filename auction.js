827f56efe4881826e4494f57bb203acdb2df36ff
NewCompte <victorvvv@gmail.com>
2016-10-16 21:01:26 +0200

    stop testing.

--- auction.js
+++ auction.js
@@ -921,7 +921,7 @@ if (Meteor.isServer) {
     console.log("Loading it up");
     // Clear state
 
-    var renewData = true || 1;
+    var renewData = false;
     var createDataSnake = false;
     var renewDataSnake = false;
     var numDraftSpots = 24;
