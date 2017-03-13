c8ee282b653fe89cdbcc96e3dd27c3abe0c02af7
NewCompte <victorvvv@gmail.com>
2017-03-13 01:03:13 +0100

    renewdata false

--- auction.js
+++ auction.js
@@ -926,7 +926,7 @@ if (Meteor.isServer) {
     console.log("Loading it up");
     // Clear state
 
-    var renewData = true;
+    var renewData = false;
     var createDataSnake = false;
     var renewDataSnake = false;
     var numDraftSpots = 24;
