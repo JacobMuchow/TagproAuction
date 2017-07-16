4a498d82be32d59f5397479632fdb3f0e5536d9b
NewCompte <victorvvv@gmail.com>
2017-07-16 12:09:16 +0200

    2nd

--- genner.py
+++ genner.py
@@ -3,26 +3,25 @@ import json
 
 ## Edit these then run the script to generate the data you need
 captains = [
-("AlHarrington", "Åball IFK", "ECLTP", 0, 0, []),
-("Battosay", "Team Battosay", "ECLTP", 0, 0, []),
-("GrammarJew", "Peräsuolijoen Palloilijat", "ECLTP", 0, 0, []),
-("Sea.", "Celtag Vigo", "ECLTP", 0, 0, []),
-("Sensei Osy", "Ball Street", "ECLTP", 0, 0, []),
-("MrSaggyBalls", "BSC Young Balls", "ECLTP", 0, 0, []),
+("AlHarrington", "Åball IFK", "ECLTP", 0, 5, ["MagicPigeon", "The Real IRS", "NewCompte", "Jim Jimson", "Player", "sweatypete", "-Jacob-", "Irish_Jesus", "SunnyMojo", "MonteCristo", "Harmony", "Fuzzy", "KBreezy" ]),
+("Battosay", "Team Battosay", "ECLTP", 0, 0, ["Dyballa", "piggeh", "Nice", "katazei", "juke' em all", "Electroball", "LoweJ", "some banter", "Daffodil", "Button", "Mathi Oi"]),
+("GrammarJew", "Peräsuolijoen Palloilijat", "ECLTP", 0, 0, ["MagicPigeon", "The Real IRS", "NewCompte", "Jim Jimson", "Player", "sweatypete", "-Jacob-", "Irish_Jesus", "SunnyMojo", "MonteCristo", "Harmony", "Fuzzy", "KBreezy"]),
+("Sea.", "Celtag Vigo", "ECLTP", 0, 0, ["Jerry.", "Chuck_Finley", "zeeres", "n00b", "Sunny", "Huck and D", "anime addict", "J Belfort", "Squirrely", "dodsfall", "Nuts!", "Juke Dough", "phoenixx", "ParvoB19"]),
+("Sensei Osy", "Ball Street", "ECLTP", 0, 0, ["Muccy", "Poukie", "Vincent Osy", "wowah", "Caro-Kann", "Wait", "ElMiracle", "Rojale", "B.Fraser", "JasonGenova.", "Bouh!", "themagpie"]),
+("MrSaggyBalls", "BSC Young Balls", "ECLTP", 0, 5, ["Fat", "Nilus", "Comakip", "NZ.", "WishICared", "eggdog", "damn, son!", "ClutchHunter", "teoretyczny", "SoapyMcguire", "Ghaul", "Phreak", "Mattyicy"]),
 ]
- 
+
 managers = [
-("bhayward2000", "Chasterfield FC", "ECLTP", 0, 0, [], "Sherrattinho"),
-("Ballkenende", "Ballmere City", "ECLTP", 0, 0, [], "kutrebar"),
-("Unplanned", "unny's Flagettis", "ECLTP", 0, 0, [], "ethce"),
-("rickastley", "RickRollers", "ECLTP", 0, 0, [], "Nube"),
+("bhayward2000", "Chasterfield FC", "ECLTP", 0, 0, ["Osy", "Heisy", "Ruud", "Sisu", "The Juker", "Koekjes", "Vjeze", "LotsO'Huggin", "ruff", "Troupee", "EASHY", "Bustaball", "Ball Grylls"], "Sherrattinho"),
+("Ballkenende", "Ballmere City", "ECLTP", 0, 5, ["Green", "Strategio", "Ballkenende", "weisbrot", "Nayr", "Sam4096", "Testi", "Jolt", "alchemist", "ALCAEUS", "Gummi", "Such A Nerd", "GrabMachine", "Triple A"], "kutrebar"),
+("Unplanned", "unny's Flagettis", "ECLTP", 0, 0, ["Sam-", "unvrs", "MikeC", "Bezeball", "Wu", "MoGGee", "schwenks", "Muzza", "Tagnoob", "Kodiak", "EphewSeaKay", "Nooga"], "ethce"),
+("rickastley", "RickRollers", "ECLTP", 0, 5, ["Muccy", "Poukie", "Vincent Osy", "wowah", "Caro-Kann", "Wait", "ElMiracle", "Rojale", "B.Fraser", "JasonGenova.", "Bouh!", "themagpie"], "Nube"),
 ]
 
 starting_money = 100
-keeper_money = 0
+keeper_money = 5
 team_size = 4
 
-keepers = False
 nominations = [{"name" : "nextInOrder", "nextorder" : 0}]
 team_names = []
 keepers = []
