69896f752e02621c95a575c8f49803df45d078de
NewCompte <victorvvv@gmail.com>
2017-07-16 21:00:14 +0200

    rick...

--- genner.py
+++ genner.py
@@ -17,7 +17,7 @@ managers = [
 ("bhayward2000", "Chaseterfield FC", "ECLTP", 0, 0, ["Osy", "Heisy", "Ruud", "Sisu", "The Juker", "Koekjes", "Vjeze", "LotsO'Huggin", "ruff", "Troupee", "EASHY", "Bustaball", "Ball Grylls"], "Sherrattinho"),
 ("Ballkenende", "Ballmere City", "ECLTP", 0, 5, ["Green", "Strategio", "Ballkenende", "weisbrot", "Nayr", "Sam4096", "Testi", "Jolt", "alchemist", "ALCAEUS", "Gummi", "Such A Nerd", "GrabMachine", "Triple A"], "kutrebar"),
 ("Unplanned", "unny's Flagettis", "ECLTP", 0, 0, ["Sam-", "unvrs", "MikeC", "Bezeball", "Wu", "MoGGee", "schwenks", "Muzza", "Tagnoob", "Kodiak", "EphewSeaKay", "Nooga"], "ethce"),
-("rickastley", "RickRollers", "ECLTP", 0, 5, ["Muccy", "Poukie", "Vincent Osy", "wowah", "Caro-Kann", "Wait", "ElMiracle", "Rojale", "B.Fraser", "JasonGenova.", "Bouh!", "themagpie"], "Nube"),
+("rickastley", "The Rickrollers", "ECLTP", 0, 5, ["Muccy", "Poukie", "Vincent Osy", "wowah", "Caro-Kann", "Wait", "ElMiracle", "Rojale", "B.Fraser", "JasonGenova.", "Bouh!", "themagpie"], "Nube"),
 ]
 
 starting_money = 100
