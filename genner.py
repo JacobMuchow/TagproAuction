47def5f5ffd8e342aaa46ded82045d5ef5ca6278
Jacob Muchow <jacobamuchow@gmail.com>
2017-10-29 19:33:17 -0500

    Ball God replaced dodsfall.

--- genner.py
+++ genner.py
@@ -19,7 +19,7 @@ captains = [
     # Ron Hextball manager
     ("eee", "The RePublicans", "Southeast", 15, 0, []),
     # JDoeMonopoly manager
-    ("dodsfall", "A Developmental Lad", "Southeast", 0, 0, []),
+    ("Ball God", "Chase Jam", "Southeast", 0, 0, []),
 
     ("Bamboozler", "877CAPSNOW", "West", 5, 0, []),
     ("Warriors", "Centra of Attention", "West", 0, 0, []),
